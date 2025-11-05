18b00a815a3e3272ceb613367a2224173465705f
HFAJIR <hamzafagire@gmail.com>
2025-11-05 11:27:37 +0100

    completed XP

--- Week1/DAY 3/Exercises/Exercises_XP/exercice4.py
+++ Week1/DAY 3/Exercises/Exercises_XP/exercice4.py
@@ -1,3 +1,14 @@
 
-git commit -m "Completed exercice4.py and made changes to Challenge1.py and test.py"
-git push origin main
+users = ["Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+
+# 1. Using dictionary comprehension
+char_to_index = {character: i for i, character in enumerate(users)}
+print(char_to_index)
+
+# 2. Using dictionary comprehension
+index_to_char = {i: character for i, character in enumerate(users)}
+print(index_to_char)
+
+# 3. Using dictionary comprehension with sorted list
+sorted_char_to_index = {character: i for i, character in enumerate(sorted(users))}
+print(sorted_char_to_index)
